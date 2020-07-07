1b383ab3c57ceea789e2272bb58a3de3bcd57016
dnejdzlr2 <dnejdzlr2@PC-721>
2020-07-07 13:44:11 +0900

    20200707 게시글 댓글달기, 수정 기능까지 완료
    https://congsong.tistory.com/32?category=749196

---
+++ src/main/java/com/board/domain/CommentDTO.java
@@ -0,0 +1,19 @@
+package com.board.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CommentDTO extends CommonDTO{
+
+	private Long idx;
+	
+	private Long boardIdx;
+	
+	private String content;
+	
+	private String writer;
+}
